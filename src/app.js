9efd0da7051beb39af95061cc399ab426a579d6f
unknown <mohitbiddu1@gmail.com>
2020-03-29 16:35:06 +0530

    footer and header created

--- src/app.js
+++ src/app.js
@@ -1,14 +1,34 @@
 const express=require("express")
 const path=require("path")
 const app=express()
+const hbs=require("hbs")
 
 const publicDirpath = path.join(__dirname,"../public")
+const viewpath=path.join(__dirname,"../templates/views")
+const partialspath=path.join(__dirname,"../templates/partials")
 
 app.use(express.static(publicDirpath))
 app.set("view engine","hbs")
+app.set("views",viewpath)
+hbs.registerPartials(partialspath)
 
 app.get("",(req,res)=>{
-    res.render("index")
+    res.render("index",{
+        title:"Homepage",
+        creator:"Mohit"
+    })
+})
+app.get("/contact",(req,res)=>{
+    res.render("contact",{
+        title:"contact",
+        creator:"Mohit"
+    })
+})
+app.get("/about",(req,res)=>{
+    res.render("about",{
+        title:"about",
+        creator:"Mohit"
+    })
 })
 app.listen(3000,()=>{
     console.log("server is up on the port 3000")
