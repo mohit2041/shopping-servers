cad0f9cc4b452e24f520d88391b27630c1c139e1
unknown <mohitbiddu1@gmail.com>
2020-03-31 15:52:14 +0530

    image now showing up

--- routes/shop.js
+++ routes/shop.js
@@ -2,7 +2,7 @@ const express=require("express")
 const router=express.Router()
 
 router.get("/",(req,res)=>{
-    res.render("../templates/views/layout",{
+    res.render("layout",{
         title:"Homepage",
         creator:"Mohit"
     })
