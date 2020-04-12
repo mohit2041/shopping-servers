f922ff42b54b49f5d772a667b1b1d79fd8135e58
mohit2041 <mohitbiddu1@gmail.com>
2020-04-12 15:56:58 +0530

    req not working on login page

--- routes/user.js
+++ routes/user.js
@@ -6,8 +6,12 @@ router.get("/login",async(req,res)=>{
     try{
         res.render("signup")
     }catch(e){
-        res.send()
+        res.status(400).send()
     }
+})
+
+router.post("/login",async(res,req)=>{
+    console.log(req.body)
 })
 
 module.exports = router
--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express=require("express")
 const path=require("path")
 const app=express()
+const bodyParser = require("body-parser")
 require("./mongoose")
 const hbs=require("hbs")
 
@@ -18,8 +19,11 @@ app.set("views",viewpath)
 hbs.registerPartials(partialspath)
 
 app.use(express.json())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded())
 app.use(itemrouter)
 app.use(userRouter)
+
 
 app.listen(3000,()=>{
     console.log("server is up on the port 3000")
